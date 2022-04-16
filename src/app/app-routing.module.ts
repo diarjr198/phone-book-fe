24440a46929215ff0024d3ad7f61c43f74ea93c3
Diar Julianto Rahadi <diar.student@gmail.com>
2022-04-16 15:03:23 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './component/add/add.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { FavoritesComponent } from './component/favorites/favorites.component';
+import { LoginComponent } from './component/login/login.component';
+import { RecentComponent } from './component/recent/recent.component';
+import { RegisterComponent } from './component/register/register.component';
+import { ViewComponent } from './component/view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', redirectTo: '/login', pathMatch: 'full' },
+	{ path: 'login', component: LoginComponent },
+	{ path: 'register', component: RegisterComponent },
+	{ path: 'contact', component: ContactComponent },
+	{ path: 'favorites', component: FavoritesComponent },
+	{ path: 'recent', component: RecentComponent },
+	{ path: 'add', component: AddComponent },
+	{ path: 'view', component: ViewComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FavoritesComponent } from './component/favorites/favorites.component';
+import { RecentComponent } from './component/recent/recent.component';
+import { AddComponent } from './component/add/add.component';
+import { ViewComponent } from './component/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContactComponent,
+    FavoritesComponent,
+    RecentComponent,
+    AddComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
