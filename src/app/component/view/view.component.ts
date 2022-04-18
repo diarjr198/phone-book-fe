c430e9cbb38afcfd6856f61de08023a11315ac5e
Diar Julianto Rahadi <diar.student@gmail.com>
2022-04-18 19:35:57 +0700

    Add component view

--- src/app/component/view/view.component.ts
+++ src/app/component/view/view.component.ts
@@ -5,6 +5,12 @@ import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faBuilding } from '@fortawesome/free-regular-svg-icons';
 import { faTools } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ContactService } from 'src/app/_services/contact.service';
+import { TokenStorageService } from 'src/app/_services/token-storage.service';
+import { Contact } from 'src/app/_models/Contact';
 
 @Component({
 	selector: 'app-view',
@@ -18,8 +24,65 @@ export class ViewComponent implements OnInit {
 	faBuilding = faBuilding;
 	faTools = faTools;
 	faEnvelope = faEnvelope;
+	faEdit = faEdit;
+	faTrash = faTrash;
 
-	constructor() {}
+	contact: Contact = {};
+	id: string = '';
+	isLoggedIn = false;
+	isEdit = false;
 
-	ngOnInit(): void {}
+	form: any = {
+		name: null,
+		phone: null,
+		email: null,
+		company: null,
+		job: null
+	};
+
+	constructor(
+		private contactService: ContactService,
+		private tokenStorage: TokenStorageService,
+		private router: Router,
+		private route: ActivatedRoute
+	) {}
+
+	ngOnInit(): void {
+		this.isLoggedIn = !!this.tokenStorage.getToken();
+
+		if (!this.isLoggedIn) {
+			this.router.navigate([ 'login' ]);
+		}
+		this.route.params.subscribe((params) => {
+			// console.log(params); //log the entire params object
+			// console.log(params['id']); //log the value of id
+			this.id = params['id'];
+		});
+
+		this.contactService.getContactSpecific(this.id).subscribe((data) => {
+			this.contact = data;
+		});
+	}
+
+	onEdit(): void {
+		this.isEdit = true;
+	}
+
+	onDelete(): void {
+		this.contactService.deleteContact(this.id).subscribe((data) => {
+			console.log('berhasil');
+			setTimeout(() => {
+				this.router.navigate([ 'contact' ]);
+			}, 500);
+		});
+	}
+
+	onSubmit(id_contact: string, name: string, phone: string, email: string, company: string, job: string): void {
+		this.contactService.updateContact(id_contact, name, phone, email, company, job).subscribe((data) => {
+			console.log('berhasil');
+			setTimeout(() => {
+				window.location.reload();
+			}, 500);
+		});
+	}
 }
