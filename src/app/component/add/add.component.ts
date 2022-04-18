8e24f19d145a1b5a226b66689fa9059eec2891f8
Diar Julianto Rahadi <diar.student@gmail.com>
2022-04-18 18:28:49 +0700

    Add component add

--- src/app/component/add/add.component.ts
+++ src/app/component/add/add.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { faUser as faUserSolid } from '@fortawesome/free-solid-svg-icons';
+import { ContactService } from 'src/app/_services/contact.service';
 
 @Component({
 	selector: 'app-add',
@@ -11,7 +13,30 @@ export class AddComponent implements OnInit {
 	faArrowLeft = faArrowLeft;
 	faUserSolid = faUserSolid;
 
-	constructor() {}
+	form: any = {
+		name: null,
+		phone: null,
+		email: null,
+		company: null,
+		job: null
+	};
+
+	constructor(private contactService: ContactService, private router: Router) {}
 
 	ngOnInit(): void {}
+
+	onSubmit(): void {
+		const { name, phone, email, company, job } = this.form;
+		this.contactService.addContact(name, phone, email, company, job).subscribe(
+			(data) => {
+				console.log('berhasil');
+				setTimeout(() => {
+					this.router.navigate([ 'view/' + data._id ]);
+				});
+			},
+			(err) => {
+				console.log('gagal');
+			}
+		);
+	}
 }
